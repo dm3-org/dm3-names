8cac564ef236d25f37792093f784a612f38569f3
AlexNi245 <alex.plutta@googlemail.com>
2024-03-22 12:14:31 +0100

    use hardhat upgradable to deploy proxy contract

--- dm3-names/deploy_l2/00_dm3NameRegistrar.ts
+++ dm3-names/deploy_l2/00_dm3NameRegistrar.ts
@@ -1,27 +1,29 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { ethers } from 'ethers';
+import { ethers, upgrades } from 'hardhat';
+
+import { ethers as ethersT } from 'ethers';
+
+
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
 
-  const { deployer } = await getNamedAccounts();
+  const Factory = await ethers.getContractFactory('Dm3NameRegistrar');
 
   const PARENT_DOMAIN = process.env.PARENT_DOMAIN;
   if (!PARENT_DOMAIN) throw 'Set $PARENT_DOMAIIN';
   console.log({ PARENT_DOMAIIN: PARENT_DOMAIN });
 
   const parentNode = ethers.namehash(PARENT_DOMAIN);
+  console.log({ parentNode });
 
-  const deployment = await deploy('Dm3NameRegistrar', {
-    from: deployer,
-    args: [parentNode],
-    log: true,
-    proxy: true,
-  });
+  const dm3NameRegistrar = await upgrades.deployProxy(Factory, [parentNode]);
+  await dm3NameRegistrar.waitForDeployment();
 
-  console.log(`Dm3NameRegistrar is deployed at ${deployment.address}`);
+  console.log(
+    `Dm3NameRegistrar is deployed at ${await dm3NameRegistrar.getAddress()}`
+  );
 };
 export default func;
 func.tags = ['Dm3NameRegistrar'];
